ecd1e79f96128061f5449ba7bd0bcae85861522a
Alen Sankovic <alen.sankovic@ibmix.hr>
2024-11-02 13:56:42 +0100

    add initial project structure

---
+++ race-application-command-service/src/main/java/com/asankovic/race/command/RaceApplicationCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.asankovic.race.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationCommandServiceApplication.class, args);
+	}
+
+}
---
+++ race-application-query-service/src/main/java/com/asankovic/race/query/RaceApplicationQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.asankovic.race.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationQueryServiceApplication.class, args);
+	}
+
+}
